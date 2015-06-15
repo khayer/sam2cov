da56881486320a4037553855b85e7ce9008d7292
Katharina Hayer <katharinaehayer@gmail.com>
2015-06-15 17:20:25 -0400

    hitting the pause button

--- sam2cov.c
+++ sam2cov.c
@@ -16,8 +16,8 @@
 //#define VERSION "v0.0.2-beta - 3/20/14"
 //#define VERSION "v0.0.3-beta - 4/15/14"
 //#define VERSION "v0.0.4-beta - 4/29/14"
-//#define VERSION "v0.0.5.1-beta - 6/15/15"
-#define VERSION "v0.0.5.2-beta - 6/20/15"
+#define VERSION "v0.0.5.1-beta - 6/15/15"
+#define VERSION "v0.0.5.2-beta - 6/15/15"
 
 void usage() {
   printf("Usage: sam2cov [OPTIONS] fai_file sam_file\n" );
